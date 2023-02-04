82fff6dbb2b9867943f3b9c7e2503102fa52663f
Rohit Tambakhe <rohit.tambakhe@live.com>
2023-02-05 00:57:45 +0530

    Formatting.

--- ECR_Auto_Scanning_Python/index.py
+++ ECR_Auto_Scanning_Python/index.py
@@ -24,7 +24,7 @@ def get_all_repos():
 def get_latest_img_digest(repo_name):
     get_latest_image_cmd = f"aws ecr describe-images --repository-name  {repo_name}  --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageDigest'"
     get_latest_image = subprocess.Popen(get_latest_image_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-    image_Digest = get_latest_image.stdout.read().decode('utf-8') 
+    image_Digest = get_latest_image.stdout.read().decode('utf-8')
     #print(f'image_Digest: {image_Digest}')
     return image_Digest
 
@@ -48,7 +48,7 @@ def get_scan_result(repo_name, image_Digest):
 
 def finding_vulnarablity(repo_name, json_finding_result):
     print_to_json( json_finding_result['imageScanFindings']['findings'])
-    for result in json_finding_result['imageScanFindings']['findings']:      
+    for result in json_finding_result['imageScanFindings']['findings']:
         if(result['severity'] == "CRITICAL"):
             item  = Vulnurability()
             item.repo_name = repo_name
@@ -56,7 +56,7 @@ def finding_vulnarablity(repo_name, json_finding_result):
             list_critical.append(item)
             attributes = result['attributes']
 
-            for att in attributes:                    
+            for att in attributes:
                 if(att["key"] == "package_name"):
                     item.pack_name = att["value"]
                 if(att["key"] == "package_version"):
@@ -68,8 +68,8 @@ def write_to_csv(list_critical):
         with open("repo_with_CRITICAL_issues.csv", 'w', newline='', encoding='utf8') as f:
             writer = csv.writer(f)
             writer.writerow(csv_columns)
-            for ob in list_critical:  
-                row = [ob.repo_name, ob.pack_name,ob.pack_version,ob.uri]        
+            for ob in list_critical:
+                row = [ob.repo_name, ob.pack_name,ob.pack_version,ob.uri]
                 writer.writerow(row)
 
 def print_to_json(data):
@@ -86,15 +86,15 @@ def main():
         print (f'------------- Starting scanning : {repo_name}')
 
         # get latest image for the repo
-        image_Digest = get_latest_img_digest(repo_name)       
+        image_Digest = get_latest_img_digest(repo_name)
 
         print(f'------------- Started the scanning: {repo_name}')
         start_scanning(repo_name, image_Digest)
-    
+
         # waiting scan result
         print(f'------------- Waiting to finish the scanning: {repo_name}')
         wait_scan_results(repo_name, image_Digest)
-        
+
         # get scan results
         json_finding_result = get_scan_result(repo_name, image_Digest)
 
@@ -103,15 +103,15 @@ def main():
         if(status == "FAILED"):
             print(f'>>>>>>>>>>>>>>>>>>>Failed to scan the repo: {repo_name}')
             continue
-    
-        # finding vulnarabilities 
+
+        # finding vulnarabilities
         finding_vulnarablity(repo_name, json_finding_result)
 
         # write all critical issues to csv file
         write_to_csv(list_critical)
 
     print(f'Repos with issues: {len(list_critical)}')
-    print(f'=====================================================') 
+    print(f'=====================================================')
 
 if __name__ == "__main__":
     main()
